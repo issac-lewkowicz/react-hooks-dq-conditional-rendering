b58a696af842617754956d3d59645e182065e11a
Yitzchak Lewkowicz <yitzchaklewkow@yahoo.com>
2022-05-23 11:25:43 -0600

    finished

--- src/components/MainBox.js
+++ src/components/MainBox.js
@@ -1,25 +1,32 @@
-import React from "react";
+import { useState } from "react";
 import MenuBar from "./MenuBar";
 import { Profile, Photos, Cocktails, Pokemon } from "./pages";
-
 function MainBox() {
+  const [menuItem, setMenuItem ] = useState("Profile")
+  const menuMap = {
+    Profile: <Profile/>,
+    Photos: <Photos/>,
+    Cocktails: <Cocktails/>,
+    Pokemon: <Pokemon/>
+  }
+  const handleClick = (selection, e) => {
+    setMenuItem(selection)
+    // e.target.className = "item active"
+  }
   /*
-    Replace the code below! Depending on what menu item is selected in the menu, 
+    Replace the code below! Depending on what menu item is selected in the menu,
     I should render either a Profile, Photos, Cocktails, or Pokemon component.
-    Think of a way to track which menu item was selected. 
-    - Which component should have state? 
-    - Which component should have methods to control state? 
+    Think of a way to track which menu item was selected.
+    - Which component should have state?
+    - Which component should have methods to control state?
     - Where should these methods be called?
   */
-
-  let detailsToDisplay = <div>Hi, I'm a div!</div>;
-
+  let detailsToDisplay = <div> {menuMap[menuItem]} </div>;
   return (
     <div>
-      <MenuBar />
+      <MenuBar clickHandle= {handleClick} selected = {menuItem}/>
       {detailsToDisplay}
     </div>
   );
 }
-
 export default MainBox;
--- src/components/MenuBar.js
+++ src/components/MenuBar.js
@@ -1,36 +1,28 @@
 import React from "react";
-
-function MenuBar(props) {
+function MenuBar({clickHandle, selected}) {
   /*
-
-  The 'span' tags below are the menu items. Think about the way a menu 
+  The 'span' tags below are the menu items. Think about the way a menu
   should work. When you click a menu item, the button typically becomes
   'active' to indicate that it is currently selected. How could we achieve
   this programatically? What other behavior do we expect when we click
   on a menu item? Do we need state in this component, and if not, how can
   this component be made aware of what is currently the active menu item?
-
   */
-
   return (
     <div className="ui four item menu">
-      <span className="item active">
+      <span className= {selected === "Profile" ? "item active" : "item"} onClick={(e)=>clickHandle("Profile", e) } >
         <i className="user large icon" />
       </span>
-
-      <span className="item">
+      <span className={selected === "Photos" ? "item active" : "item"} onClick={(e)=>clickHandle("Photos", e)}>
         <i className="photo large icon" />
       </span>
-
-      <span className="item">
+      <span className={selected === "Cocktails" ? "item active" : "item"}onClick={(e)=>clickHandle("Cocktails", e)}>
         <i className="cocktail large icon" />
       </span>
-
-      <span className="item">
+      <span className={selected === "Pokemon" ? "item active" : "item"} onClick={(e)=>clickHandle("Pokemon", e)}>
         <i className=" themeisle large icon" />
       </span>
     </div>
   );
 }
-
 export default MenuBar;
